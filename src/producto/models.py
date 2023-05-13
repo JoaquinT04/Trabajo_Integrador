3314b99bea56bbb98906daee6ae8d3263a3fdce2
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2023-05-13 12:08:20 -0300

    Modelos de Producto y Proveedor

--- src/producto/models.py
+++ src/producto/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+	nombre = models.CharField(max_length=50)
+	precio= models.FloatField()
+	stock_actual = models.IntegerField()
+	proveedor = models.ForeignKey(Proveedor,on_delete=models.CASCADE)
+	
+	def __str__(self) -> str:
+		return f"{self.nombre.capitalize} {self.precio} stock: {self.stock_actual}"
+	
+	class Meta:
+		ordering=["stock_actual"]
--- src/proveedor/models.py
+++ src/proveedor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+	nombre = models.CharField(max_length=50)
+	apellido = models.CharField(max_length=50)
+	dni = models.PositiveIntegerField()
+
+	def __str__(self) -> str:
+		return f"{self.nombre} {self.apellido} {self.dni}"
+	
+	class Meta:
+		ordering = ["apellido","nombre","dni"]
